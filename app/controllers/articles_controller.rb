954d2ebd45efd69a460fa7ff836e181607987342
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-21 16:33:04 +0000

    some crud changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
-class ArticlesController < ApplicationController
+ class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -13,8 +21,25 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update 
+        @article = Article.find(params[:id]);
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully update"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  root 'pages#home'
+
+  get '/about', to: 'pages#about'
+
   resources :articles
 
 end
