c91dd2d5f83a57c0b39dc0b9323c62e4875624c5
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-21 16:37:17 +0000

    some changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,16 @@
  class ArticlesController < ApplicationController
+   before_action :set_article, only: [:edit, :update, :show, :destroy]
+   
   def index
     @articles = Article.all
   end
   
-  def new
+  def newgit
     @article = Article.new
   end
   
   def edit
-    @article = Article.find(params[:id])
+
   end
   
   def create
@@ -22,7 +24,7 @@
   end
   
   def update 
-        @article = Article.find(params[:id]);
+
     if @article.update(article_params)
       flash[:notice] = "Article was succesfully update"
       redirect_to article_path(@article)
@@ -32,17 +34,20 @@
   end
   
   def show
-    @article = Article.find(params[:id])
+
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was succesfully deleted"
     redirect_to articles_path
   end
   
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
