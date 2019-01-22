96e1dafbea13a3fba187173835414ad864b6d197
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-22 13:22:09 +0000

    adding users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@
   def create
     @article = Article.new(article_params);
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -26,7 +26,7 @@
   def update 
 
     if @article.update(article_params)
-      flash[:notice] = "Article was succesfully update"
+      flash[:success] = "Article was succesfully update"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -39,7 +39,7 @@
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article was succesfully deleted"
+    flash[:danger] = "Article was succesfully deleted"
     redirect_to articles_path
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :comments
+  resources :users
   root 'pages#home'
 
   get '/about', to: 'pages#about'
