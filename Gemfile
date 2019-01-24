06d914b24665b7a61db1a990ae6154cb7cf29c2e
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-24 08:38:05 +0000

    some integration test for listing categories

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'rails-controller-testing'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   
-  resources :comments
-  resources :users, except: [:new]
   root 'pages#home'
-
   get '/about', to: 'pages#about'
 
+  resources :comments
+  resources :users, except: [:new]
   resources :articles
+  resources :categories, except: [:destroy]
 
   get 'signup', to: 'users#new'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
 end
