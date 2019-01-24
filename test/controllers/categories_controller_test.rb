b5256d60c6b558a20b4f04a16963d2a772fe53d1
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-24 13:23:27 +0000

    fix test and update navbar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "testadmin", email: "test@admin.com", password: "testadmin", admin: true)
   end
   
   test "should get categories index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get new" do
+    sign_in_as(@user, "testadmin")
     get new_category_path
     assert_response :success
   end
@@ -18,5 +20,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
