2eea3a852785a9d89b707aca0b5cdcce74a38c61
EC2 Default User <ec2-user@ip-172-31-46-224.us-east-2.compute.internal>
2019-01-24 14:24:31 +0000

    adding articles category front

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,8 @@ class CategoriesController < ApplicationController
   end
   
   def show
-  
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def new
